8b1df3fe8433f4a3214dfbe10acc25678881bd48
hxj <53702200+hxj0x@users.noreply.github.com>
2023-09-14 14:43:50 +0000

    保序，filter

---
+++ src/solution/s0026_remove_duplicates_from_sorted_array.rs
@@ -0,0 +1,35 @@
+pub struct Solution {}
+
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let len = nums.len();
+        if len <= 1 {
+            return len as i32;
+        }
+        let mut slow = 0;
+        for fast in 1..len {
+            if nums[slow] != nums[fast] {
+                slow += 1;
+                nums[slow] = nums[fast];
+            }
+        }
+        nums.truncate(slow + 1);
+        return (slow + 1) as i32;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_26() {
+        assert_eq!(Solution::remove_duplicates(&mut vec![]), 0);
+        let mut vec1 = vec![1, 1, 1, 1, 3];
+        assert_eq!(Solution::remove_duplicates(&mut vec1), 2);
+        assert_eq!(vec1, vec![1, 3]);
+        let mut vec2 = vec![1, 1, 2];
+        assert_eq!(Solution::remove_duplicates(&mut vec2), 2);
+        assert_eq!(vec2, vec![1, 2]);
+    }
+}
